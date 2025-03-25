1db4bf91499f9f08adba5a2ee7de89e588cd0300
subas-roy <subasroy46@gmail.com>
2025-03-25 11:07:18 +0600

    Invalid previous cache after mutation, consume updated cache and display in realtime

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,10 +3,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }),
-  tagTypes: ['task'],
+  tagTypes: ['task'], // Name the cached data - 1
   endpoints: (builder) => ({
     getTasks: builder.query({
       query: () => '/tasks',
+      providesTags: ['task'], // Name the cached data as task while fetching data - 2
     }),
     createTask: builder.mutation({
       query: (taskData) => ({
@@ -14,6 +15,7 @@ export const baseApi = createApi({
         method: 'POST',
         body: taskData,
       }),
+      invalidatesTags: ['task'], // Invalid the previous cached data after mutation - 3
     }),
   }),
 });
